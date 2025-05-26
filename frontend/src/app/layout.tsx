b6a25d282c5578a06c94ac9e4859c89435b296b1
Harsh Prajapati <harshpraj21@gmail.com>
2025-05-27 03:16:13 +0530

    Razorpay script added in frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,10 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Script
+        src="https://checkout.razorpay.com/v1/checkout.js"
+        strategy="beforeInteractive"
+      />
     </html>
   );
 }
